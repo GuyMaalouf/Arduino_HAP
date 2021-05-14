2ae9fe7625c810a378ffd59f2fbe8e101a613ccb
The Workshop <80882855+TheWorkshopByGM@users.noreply.github.com>
2021-05-14 12:00:44 +0300

    Add files via upload

---
+++ My_Arduino_HAP_Homekit_PWM.ino
@@ -0,0 +1,113 @@
+/*
+ * temperature_sensor.ino
+ *
+ * This example shows how to:
+ * 1. define a temperature sensor accessory and its characteristics (in my_accessory.c).
+ * 2. report the sensor value to HomeKit (just random value here, you need to change it to your real sensor value).
+ *
+ *  Created on: 2020-05-15
+ *      Author: Mixiaoxiao (Wang Bin)
+ *
+ * Note:
+ *
+ * You are recommended to read the Apple's HAP doc before using this library.
+ * https://developer.apple.com/support/homekit-accessory-protocol/
+ *
+ * This HomeKit library is mostly written in C,
+ * you can define your accessory/service/characteristic in a .c file,
+ * since the library provides convenient Macro (C only, CPP can not compile) to do this.
+ * But it is possible to do this in .cpp or .ino (just not so conveniently), do it yourself if you like.
+ * Check out homekit/characteristics.h and use the Macro provided to define your accessory.
+ *
+ * Generally, the Arduino libraries (e.g. sensors, ws2812) are written in cpp,
+ * you can include and use them in a .ino or a .cpp file (but can NOT in .c).
+ * A .ino is a .cpp indeed.
+ *
+ * You can define some variables in a .c file, e.g. int my_value = 1;,
+ * and you can access this variable in a .ino or a .cpp by writing extern "C" int my_value;.
+ *
+ * So, if you want use this HomeKit library and other Arduino Libraries together,
+ * 1. define your HomeKit accessory/service/characteristic in a .c file
+ * 2. in your .ino, include some Arduino Libraries and you can use them normally
+ *                  write extern "C" homekit_characteristic_t xxxx; to access the characteristic defined in your .c file
+ *                  write your logic code (eg. read sensors) and
+ *                  report your data by writing your_characteristic.value.xxxx_value = some_data; homekit_characteristic_notify(..., ...)
+ * done.
+ */
+
+#include <Arduino.h>
+#include <arduino_homekit_server.h>
+#include "wifi_info.h"
+#include <DHT.h>
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor 
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+#define LOG_D(fmt, ...)   printf_P(PSTR(fmt "\n") , ##__VA_ARGS__);
+
+// access your homekit characteristics defined in my_accessory.c
+extern "C" homekit_server_config_t config;
+extern "C" homekit_characteristic_t cha_current_temperature;
+extern "C" homekit_characteristic_t cha_humidity;
+extern "C" void accessory_init();
+
+static uint32_t next_heap_millis = 0;
+static uint32_t next_report_millis = 0;
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+  wifi_connect(); // in wifi_info.h
+  my_homekit_setup();
+}
+
+void loop() {
+  my_homekit_loop();
+  delay(10);
+}
+
+//==============================
+// Homekit setup and loop
+//==============================
+
+void my_homekit_setup() {
+  accessory_init();
+  arduino_homekit_setup(&config);
+}
+
+void my_homekit_loop() {
+  arduino_homekit_loop();
+  const uint32_t t = millis();
+  if (t > next_report_millis) {
+    // report sensor values every 10 seconds
+    next_report_millis = t + 10 * 1000;
+    my_homekit_report();
+  }
+  if (t > next_heap_millis) {
+    // show heap info every 5 seconds
+    next_heap_millis = t + 5 * 1000;
+    LOG_D("Free heap: %d, HomeKit clients: %d",
+        ESP.getFreeHeap(), arduino_homekit_connected_clients_count());
+
+  }
+}
+
+void my_homekit_report() {
+  float temperature_value = dht.readTemperature();  
+  cha_current_temperature.value.float_value = temperature_value;
+  float humidity_value = dht.readHumidity();
+  cha_humidity.value.float_value = humidity_value;
+  LOG_D("Current temperature: %.1f", temperature_value);
+  LOG_D("Current Humidity: %.1f", humidity_value);
+  homekit_characteristic_notify(&cha_current_temperature, cha_current_temperature.value);
+  homekit_characteristic_notify(&cha_humidity, cha_humidity.value);
+}
+
+int random_value(int min, int max) {
+  return min + random(max - min);
+}
---
+++ my_accessory.c
@@ -0,0 +1,136 @@
+/*
+ * my_accessory.c
+ * Define the accessory in C language using the Macro in characteristics.h
+ *
+ *  Created on: 2020-05-15
+ *      Author: Mixiaoxiao (Wang Bin)
+ */
+
+#include <Arduino.h>
+#include <homekit/types.h>
+#include <homekit/homekit.h>
+#include <homekit/characteristics.h>
+#include <stdio.h>
+#include <port.h>
+
+#define PIN_LED  12//D6
+
+int led_bri = 100; //[0, 100]
+bool led_power = false; //true or flase
+
+// Called to identify this accessory. See HAP section 6.7.6 Identify Routine
+// Generally this is called when paired successfully or click the "Identify Accessory" button in Home APP.
+void my_accessory_identify(homekit_value_t _value) {
+  printf("accessory identify\n");
+  for (int j = 0; j < 3; j++) {
+    led_power = true;
+    led_update();
+    delay(100);
+    led_power = false;
+    led_update();
+    delay(100);
+  }
+}
+
+void led_update() {
+  if (led_power) {
+    int pwm = (int) (led_bri * 1.0 * PWMRANGE / 100.0 + 0.5f);
+    analogWrite(PIN_LED, pwm);
+    printf("ON  %3d (pwm: %4d of %d)\n", led_bri, pwm, PWMRANGE);
+  } else {
+    printf("OFF\n");
+    digitalWrite(PIN_LED, LOW);
+  }
+}
+
+void accessory_init() {
+  pinMode(PIN_LED, OUTPUT);
+  led_update();
+}
+
+homekit_value_t led_on_get() {
+  return HOMEKIT_BOOL(led_power);
+}
+
+void led_on_set(homekit_value_t value) {
+ if (value.format != homekit_format_bool) {
+    printf("Invalid on-value format: %d\n", value.format);
+    return;
+  }
+  led_power = value.bool_value;
+  if (led_power) {
+    if (led_bri < 1) {
+      led_bri = 100;
+    }
+  }
+  led_update();
+}
+
+homekit_value_t light_bri_get() {
+  return HOMEKIT_INT(led_bri);
+}
+
+void led_bri_set(homekit_value_t value) {
+  if (value.format != homekit_format_int) {
+    return;
+  }
+  led_bri = value.int_value;
+  led_update();
+}
+
+// For TEMPERATURE_SENSOR,
+// the required characteristics are: CURRENT_TEMPERATURE
+// the optional characteristics are: NAME, STATUS_ACTIVE, STATUS_FAULT, STATUS_TAMPERED, STATUS_LOW_BATTERY
+// See HAP section 8.41 and characteristics.h
+// (required) format: float; HAP section 9.35; min 0, max 100, step 0.1, unit celsius
+homekit_characteristic_t cha_current_temperature = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
+homekit_characteristic_t cha_humidity  = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
+
+homekit_accessory_t *accessories[] = {
+    HOMEKIT_ACCESSORY(.id=1, .category=homekit_accessory_category_sensor, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            &cha_current_temperature,
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),//optional
+            NULL
+        }),
+        HOMEKIT_SERVICE(HUMIDITY_SENSOR, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
+            &cha_humidity,
+            NULL
+        }),
+        NULL
+    }),
+    HOMEKIT_ACCESSORY(.id=2, .category=homekit_accessory_category_lightbulb, .services=(homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "LED Light"),
+            HOMEKIT_CHARACTERISTIC(MANUFACTURER, "Arduino HomeKit"),
+            HOMEKIT_CHARACTERISTIC(SERIAL_NUMBER, "0123456"),
+            HOMEKIT_CHARACTERISTIC(MODEL, "ESP8266/ESP32"),
+            HOMEKIT_CHARACTERISTIC(FIRMWARE_REVISION, "1.0"),
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, my_accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(LIGHTBULB, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Led"),
+            HOMEKIT_CHARACTERISTIC(BRIGHTNESS, 100, .getter=light_bri_get, .setter=led_bri_set),
+            HOMEKIT_CHARACTERISTIC(ON, false,.getter=led_on_get,.setter=led_on_set), 
+            NULL
+        }),
+        NULL
+    }),
+    NULL
+};
+
+homekit_server_config_t config = {
+    .accessories = accessories,
+    .password = "111-11-111"
+};
